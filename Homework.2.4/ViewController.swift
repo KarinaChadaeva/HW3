95d0db91b3ee1290eccc7a9b29e2f1e9d493e171
KarinaChadaeva <118630641+KarinaChadaeva@users.noreply.github.com>
2023-01-17 15:45:23 +0300

    Initial Commit

---
+++ Homework.2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework.2.4
+//
+//  Created by Карина Чадаева on 17.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
